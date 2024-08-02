6311c94c85df7b8dccb68bd7c4b7061581c8dfef
indexofmayank <maimayanmanu@gmail.com>
2024-08-02 19:21:13 +0530

    virtualdid

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,15 +1,31 @@
 const mongoose = require('mongoose');
 
-const categoryModel = mongoose.Schema({
+const categorySchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Please enter name']
     },
-
     image: {
         type: String,
         required: [true, 'Please enter category']
     }
-}, {timestamps: true});
+}, {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+});
 
-module.exports = mongoose.model('Category', categoryModel);
+// Create a virtual property 'id' that's computed from '_id'
+categorySchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+// Ensure virtual fields are serialized.
+categorySchema.set('toJSON', {
+    virtuals: true
+});
+categorySchema.set('toObject', {
+    virtuals: true
+});
+
+module.exports = mongoose.model('Category', categorySchema);
