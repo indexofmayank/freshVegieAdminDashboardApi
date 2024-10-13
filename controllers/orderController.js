0785e6016d15bb52c6c83af108d3b4114062f7d5
indexofmayank <maimayanmanu@gmail.com>
2024-10-13 14:25:31 +0530

    did for mail

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -227,159 +227,167 @@ exports.createNewOrder = catchAsyncError(async (req, res, next) => {
 
     //=-=-=-=-=-=-=-=-=-=-=-= mail option -=-=-=-=-=-=-=-=-=-=-=-=-
 
-  //   if(req.body.user.email && req.body.user) {
-  //     const items = req.body.orderItems || [];
-  //     const to = req.body.user.email;
-  //     const subject = 'Order placed at fresh Vegie'
-  //     const htmlContent = `
-  //     <!DOCTYPE html>
-  //     <html lang="en">
-  //     <head>
-  //         <meta charset="UTF-8">
-  //         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  //         <title>Order Confirmation</title>
-  //         <style>
-  //             body {
-  //                 font-family: Arial, sans-serif;
-  //                 background-color: #f4f4f4;
-  //                 margin: 0;
-  //                 padding: 0;
-  //             }
-  //             .container {
-  //                 width: 100%;
-  //                 max-width: 600px;
-  //                 margin: 0 auto;
-  //                 background-color: #ffffff;
-  //                 padding: 20px;
-  //                 border-radius: 8px;
-  //                 box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  //             }
-  //             h1 {
-  //                 color: #333;
-  //             }
-  //             .header {
-  //                 text-align: center;
-  //                 padding: 20px 0;
-  //             }
-  //             .header h1 {
-  //                 color: #4caf50;
-  //                 font-size: 28px;
-  //                 margin: 0;
-  //             }
-  //             .order-details {
-  //                 margin: 20px 0;
-  //             }
-  //             .order-details p {
-  //                 margin: 10px 0;
-  //             }
-  //             .order-details .bold {
-  //                 font-weight: bold;
-  //             }
-  //             .order-items {
-  //                 border: 1px solid #ddd;
-  //                 padding: 10px;
-  //                 margin: 20px 0;
-  //             }
-  //             .order-items h3 {
-  //                 border-bottom: 1px solid #ddd;
-  //                 padding-bottom: 10px;
-  //                 margin-bottom: 10px;
-  //                 color: #333;
-  //             }
-  //             .order-items ul {
-  //                 list-style: none;
-  //                 padding: 0;
-  //                 margin: 0;
-  //             }
-  //             .order-items li {
-  //                 padding: 10px 0;
-  //                 border-bottom: 1px solid #ddd;
-  //             }
-  //             .order-items li:last-child {
-  //                 border-bottom: none;
-  //             }
-  //             .contact-info {
-  //                 margin: 20px 0;
-  //                 padding: 20px;
-  //                 background-color: #f4f4f4;
-  //                 border-radius: 8px;
-  //                 text-align: center;
-  //             }
-  //             .contact-info p {
-  //                 margin: 0;
-  //             }
-  //             .footer {
-  //                 text-align: center;
-  //                 font-size: 12px;
-  //                 color: #888;
-  //                 margin-top: 20px;
-  //             }
-  //         </style>
-  //     </head>
-  //     <body>
-  //         <div class="container">
-  //             <div class="header">
-  //                 <h1>Order Placed Successfully!</h1>
-  //             </div>
-  //             <p>Dear <strong>${customerName}</strong>,</p>
-  //             <p>Thank you for shopping with <strong>[Your Company Name]</strong>. We're happy to inform you that your order has been placed successfully. Below are the details:</p>
-  //             <div class="order-details">
-  //                 <p class="bold">Order Number:</p> <p>${orderNumber}</p>
-  //                 <p class="bold">Order Date:</p> <p>${orderDate}</p>
-  //                 <p class="bold">Total Amount:</p> <p>${totalAmount}</p>
-  //             </div>
-  //             <div class="order-details">
-  //                 <p class="bold">Shipping Address:</p>
-  //                 <p>${shippingAddress}</p>
-  //                 <p class="bold">Estimated Delivery Date:</p>
-  //                 <p>${estimatedDeliveryDate}</p>
-  //             </div>
-  //             <div class="order-items">
-  //                 <h3>Items Ordered</h3>
-  //                 <ul>
-  //                     ${items.map(item => `<li>${item.name} - ${item.quantity} - ${item.item_price}</li>`).join('')}
-  //                 </ul>
-  //             </div>
-  //             <p>You will receive a confirmation email with tracking details once your order is shipped. You can also check the status of your order using the link below:</p>
-  //                 <p>If you have any questions, feel free to contact our support team:</p>
-  //                 <p>Email: fortune.solutionpoint@gmail.com</p>
-  //                 <p>Phone: 9935377995</p>
-  //             </div>
-  //             <div class="footer">
-  //                 <p>If you did not place this order, please contact us immediately at ${supportEmail}.</p>
-  //             </div>
-  //         </div>
-  //     </body>
-  //     </html>
-  // `;
+    if(req.body.user.email && req.body.user) {
+      const shippindAddress = [
+        req.body.shippingInfo.deliveryAddress.address,
+        req.body.shippingInfo.deliveryAddress.locality,
+        req.body.shippingInfo.deliveryAddress.landmark,
+        req.body.shippingInfo.deliveryAddress.city,
+        req.body.shippingInfo.deliveryAddress.pin_code,
+        req.body.shippingInfo.deliveryAddress.state
+      ].join(', ')
+      const items = req.body.orderItems || [];
+      const to = req.body.user.email;
+      const subject = 'Order placed at fresh Vegie for '+ result.orderId
+      const htmlContent = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Order Confirmation</title>
+          <style>
+              body {
+                  font-family: Arial, sans-serif;
+                  background-color: #f4f4f4;
+                  margin: 0;
+                  padding: 0;
+              }
+              .container {
+                  width: 100%;
+                  max-width: 600px;
+                  margin: 0 auto;
+                  background-color: #ffffff;
+                  padding: 20px;
+                  border-radius: 8px;
+                  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+              }
+              h1 {
+                  color: #333;
+              }
+              .header {
+                  text-align: center;
+                  padding: 20px 0;
+              }
+              .header h1 {
+                  color: #4caf50;
+                  font-size: 28px;
+                  margin: 0;
+              }
+              .order-details {
+                  margin: 20px 0;
+              }
+              .order-details p {
+                  margin: 10px 0;
+              }
+              .order-details .bold {
+                  font-weight: bold;
+              }
+              .order-items {
+                  border: 1px solid #ddd;
+                  padding: 10px;
+                  margin: 20px 0;
+              }
+              .order-items h3 {
+                  border-bottom: 1px solid #ddd;
+                  padding-bottom: 10px;
+                  margin-bottom: 10px;
+                  color: #333;
+              }
+              .order-items ul {
+                  list-style: none;
+                  padding: 0;
+                  margin: 0;
+              }
+              .order-items li {
+                  padding: 10px 0;
+                  border-bottom: 1px solid #ddd;
+              }
+              .order-items li:last-child {
+                  border-bottom: none;
+              }
+              .contact-info {
+                  margin: 20px 0;
+                  padding: 20px;
+                  background-color: #f4f4f4;
+                  border-radius: 8px;
+                  text-align: center;
+              }
+              .contact-info p {
+                  margin: 0;
+              }
+              .footer {
+                  text-align: center;
+                  font-size: 12px;
+                  color: #888;
+                  margin-top: 20px;
+              }
+          </style>
+      </head>
+      <body>
+          <div class="container">
+              <div class="header">
+                  <h1>Order Placed Successfully!</h1>
+              </div>
+              <p>Dear <strong>${result.user.name}</strong>,</p>
+              <p>Thank you for shopping with <strong>Fresh Vegie</strong>. We're happy to inform you that your order has been placed successfully. Below are the details:</p>
+              <div class="order-details">
+                  <p class="bold">Order Number:</p> <p>${result.orderId}</p>
+                  <p class="bold">Order Date:</p> <p>${result.createdAt}</p>
+                  <p class="bold">Total Amount:</p> <p>${result.grandTotal}</p>
+              </div>
+              <div class="order-details">
+                  <p class="bold">Shipping Address:</p>
+                  <p>${shippindAddress}</p>
+                  <p class="bold">Estimated Delivery Date:</p>
+                  <p>${result.createdAt + 1}</p>
+              </div>
+              <div class="order-items">
+                  <h3>Items Ordered</h3>
+                  <ul>
+                      ${items.map(item => `<li>${item.name} - ${item.quantity} - ${item.item_price}</li>`).join('')}
+                  </ul>
+              </div>
+              <p>You will receive a confirmation email with tracking details once your order is shipped. You can also check the status of your order using the link below:</p>
+                  <p>If you have any questions, feel free to contact our support team:</p>
+                  <p>Email: fortune.solutionpoint@gmail.com</p>
+                  <p>Phone: 9935377995</p>
+              </div>
+              <div class="footer">
+                  <p>If you did not place this order, please contact us immediately at fortune.solutionpoint@gmail.com.</p>
+              </div>
+          </div>
+      </body>
+      </html>
+  `;
   
-  //     const transporter = nodemailer.createTransport({
-  //       host: 'smtp.gmail.com',
-  //       port: 587,
-  //       secure: false,
-  //       auth: {
-  //         user: 'fortune.solutionpoint@gmail.com',
-  //         pass: 'rsyh xzdk cfgo vdak' // Consider using environment variables for this 
-  //       }
-  //     });
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+          user: 'fortune.solutionpoint@gmail.com',
+          pass: 'rsyh xzdk cfgo vdak' // Consider using environment variables for this 
+        }
+      });
   
-  //     try {
-  //       const info = await transporter.sendMail({
-  //         from: 'fortune.solutionpoint@gmail.com',
-  //         to,
-  //         subject,
-  //         html: htmlContent // Use HTML content here
-  //       });
-  //     } catch (error) {
-  //       console.error(error);
-  //       return res.status(500).json({
-  //         success: false,
-  //         message: 'Error placing order and sending email',
-  //         error: error.message
-  //       });
-  //     }
+      try {
+        const info = await transporter.sendMail({
+          from: 'fortune.solutionpoint@gmail.com',
+          to,
+          subject,
+          html: htmlContent // Use HTML content here
+        });
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+          success: false,
+          message: 'Error placing order and sending email',
+          error: error.message
+        });
+      }
 
-  //   } 
+    } 
 
     //=-=-=-=-=-=-=-=-=-=-=-= mail option -=-=-=-=-=-=-=-=-=-=-=-=-
 
