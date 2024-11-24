bb0995512cc457fd3a9b1641dec70579bbc2643c
pandeyavinash3 <63093412+pandeyavinash3@users.noreply.github.com>
2024-11-24 12:49:14 +0530

    updated controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -80,6 +80,7 @@ exports.getAllProducts = catchAsyncError(async (req, res, next) => {
   // const page = parseInt(req.query.page) || 1;
   // const limit = parseInt(req.query.limit) || 10;
   // const skip = (page - 1) * limit;
+  
   try {
     const products = await Product.aggregate([
       // {
